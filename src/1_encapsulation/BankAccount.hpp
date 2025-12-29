222b5ff90ff692c8c8d1b3bd435d2beae4276a83
Denise Reyes-Petalino <denisepetalino@gmail.com>
2025-12-29 22:53:27 +0000

    encapsulation - 6

--- src/1_encapsulation/BankAccount.hpp
+++ src/1_encapsulation/BankAccount.hpp
@@ -4,19 +4,46 @@
 using namespace std;
 
 class BankAccount{
+  private:
+  std::string accountNumber;
+  double balance;
+  //nested class
+  class Transaction{
     private:
-    std::string accountNumber;
-    double balance;
-    // nested class
-    class Transaction{
-    };
+    BankAccount& account;
+    double amount;
+
     public:
-    //constructor
-    BankAccount(const std::string& bankAccountNumber, double bankBalance);
-    //getter methods
-    std::string getAccountNumber() const;
-    double getBalance() const;
-    //setter methods
-    void setAccountNumber(const std::string& newAccountNumber);
-    void setBalance(double newBalance);
+    Transaction(BankAccount& acct, double amt) : account(acct), amount(amt){}
+
+    void deposit(){
+      if (amount <=0){
+        std::cout << "Deposit failed. Amount must be greater than 0.";
+        return;
+      }
+
+      account.balance += amount;
+      std::cout<< "Depositing: " << amount << " New balance: " << account.balance << "\n";
+    }
+
+    void withdraw(){
+      if (account.balance < amount){
+        std::cout << "Withdrawal of " << amount << " failed. Insufficient balance: " << account.balance;
+      } else {
+        std::cout<< "Current balance: " << account.balance;
+        account.balance -= amount;
+        std::cout << "Withdrawal of " << amount << " successful. New balance: " << account.balance;
+      }
+    }
+  };
+
+  public:
+  //constructor
+  BankAccount(const std::string& bankAccountNumber, double bankBalance);
+  //getter methods
+  std::string getAccountNumber() const;
+  double getBalance() const;
+  //setter methods
+  void setAccountNumber(const std::string& newAccountNumber);
+  void setBalance(double newBalance);
 };
