8702b8d89a39804bf38b490dad006bfb2aad9f2e
Denise Reyes-Petalino <denisepetalino@gmail.com>
2025-12-29 18:41:42 +0000

    encapsulation - set up bankaccount class

---
+++ src/1_encapsulation/BankAccount.cpp
@@ -0,0 +1,25 @@
+#include "BankAccount.hpp"
+
+//constructor
+BankAccount::BankAccount(const std::string& bankAccountNumber, double bankBalance) : accountNumber(bankAccountNumber), balance(bankBalance) {}
+
+//getter methods
+std::string BankAccount::getAccountNumber()
+const{
+    return accountNumber;
+}
+
+double BankAccount::getBalance()
+const{
+    return balance;
+}
+
+//setter methods
+void BankAccount::setAccountNumber(const std::string& newAccountNumber){
+    accountNumber = newAccountNumber;
+}
+
+void BankAccount::setBalance(double newBalance){
+    balance = newBalance;
+}
+
---
+++ src/1_encapsulation/BankAccount.hpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+class BankAccount{
+    private:
+    std::string accountNumber;
+    double balance;
+    
+    public:
+    //constructor
+    BankAccount( const std::string& bankAccountNumber, double bankBalance);
+    
+    //getter methods
+    std::string getAccountNumber() const;
+    double getBalance() const;
+
+    //setter methods
+    void setAccountNumber(const std::string& newAccountNumber);
+    void setBalance(double newBalance);
+};
