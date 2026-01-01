4723e5a045a32f42cd0e9d6c8cdc3317fbe14661
Denise Reyes-Petalino <denisepetalino@gmail.com>
2026-01-01 18:33:48 +0000

    6

--- src/3_polymorphism/Character.cpp
+++ src/3_polymorphism/Character.cpp
@@ -1,7 +1,19 @@
 #include "Character.hpp"
 
+using namespace std;
+
 Character::Character(){
   this->name = "Elden King";
   this->health = 100;
   this->power = 10;
+}
+
+void Character::attack(){
+  cout << "Basic Low Attack" << endl;
+}
+
+void Character::displayInfo(){
+  cout << "Name: " << name << endl;
+  cout << "Health: " << health << endl;
+  cout << "Power: " << power << endl;
 }
